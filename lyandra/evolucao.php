3cae154528d35d31112e35698d395d83034c5edd
Lyly <lyandracruz.sesisenai@gmail.com>
2024-11-28 10:08:44 -0300

    ....

--- lyandra/evolucao.php
+++ lyandra/evolucao.php
@@ -122,7 +122,7 @@
         <h2>Projeto evolução</h2>
         <h2>☸ HUMANAS ☸</h2>
         <p>"Até os momentos de silêncio carregam a força de uma obra inacabada."</p>
-        <li><a href="../index.php">Voltar para pagina incial</a></li>
+        <li><a href="../index.php" class="brilho">Voltar para página inicial</a></li>
     </section>
 
 
