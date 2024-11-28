c26bff4727740acbc67b3a0eaa8772b12f8e8e2f
Lyly <lyandracruz.sesisenai@gmail.com>
2024-11-28 07:46:49 -0300

    fim

--- lyandra/evolucao.php
+++ lyandra/evolucao.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Legenda ao Lado</title>
+    <title>Lyandra</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
